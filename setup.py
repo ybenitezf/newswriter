cfa8678c3318e273742b550e048ac2331ffc1bd4
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-01-30 10:09:33 -0500

    Bump to version 0.0.3->0.0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='newswriter',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/ybenitezf/newswriter',
     license='GPL',
     author='Yoel Benítez Fonseca',
