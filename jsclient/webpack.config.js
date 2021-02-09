44d1d94fd7114aa3d7957155d04e7cb748ccb5d7
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-02-09 11:31:17 -0500

    Used autogenerated template for static's import
    Related to #8

--- jsclient/webpack.config.js
+++ jsclient/webpack.config.js
@@ -1,4 +1,5 @@
-const path = require( 'path' );
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require( 'path' );
 
 module.exports = {
     context: __dirname,
@@ -9,6 +10,12 @@ module.exports = {
         path: path.resolve( __dirname, '../newswriter/static/js' ),
         filename: '[name].js',
     },
+    plugins: [new HtmlWebpackPlugin({
+        inject: false,
+        template: 'src/jsimports.ejs',
+        publicPath: '',
+        filename: path.resolve( __dirname, '../newswriter/templates/jsfiles.html'),
+    })],
     module: {
         rules: [
             {
@@ -24,5 +31,26 @@ module.exports = {
     },
     externals: {
       jquery: 'jQuery'
-    }
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 0,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+    
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `vendor.${packageName.replace('@', '')}`;
+              },
+            },
+          },
+        }
+    },
 };
