01efe1124ce0bd5bea3dbadd002e5303d87f93bf
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-02-18 09:12:49 -0500

    Bump version: 0.0.5 → 0.0.6

--- newswriter/__init__.py
+++ newswriter/__init__.py
@@ -16,7 +16,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 import pathlib
 import os
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 logs = LogSetup()
 db = SQLAlchemy()
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='newswriter',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/ybenitezf/newswriter',
     license='GPL',
     author='Yoel Benítez Fonseca',
