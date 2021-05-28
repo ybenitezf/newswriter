4ba02c30e9c6e2611ce45f2b3afac19f59fcabb5
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-05-28 09:42:15 -0400

    Bump version: 0.1.0 → 0.2.0

--- newswriter/__init__.py
+++ newswriter/__init__.py
@@ -19,7 +19,7 @@ import os
 import datetime
 import sys
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 logs = LogSetup()
 db = SQLAlchemy()
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='newswriter',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/ybenitezf/newswriter',
     license='GPL',
     author='Yoel Benítez Fonseca',
